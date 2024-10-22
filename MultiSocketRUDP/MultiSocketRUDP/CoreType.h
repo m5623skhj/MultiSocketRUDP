1a5a175e351396bd292e7abe63dd95e272a9a582
m5623skhj <m5623skhj@gmail.com>
2024-10-23 01:53:19 +0900

    * RUDPSessionBroker 파일 분리 * 필요한 코어 타입들 추가

--- MultiSocketRUDP/MultiSocketRUDP/CoreType.h
+++ MultiSocketRUDP/MultiSocketRUDP/CoreType.h
@@ -1,1 +1,7 @@
 #pragma once
+
+using PortType = unsigned short;
+constexpr PortType invalidPort = -1;
+
+using SessionIdType = unsigned short;
+constexpr SessionIdType invalidSession = -1;
---
+++ MultiSocketRUDP/MultiSocketRUDP/RUDPSessionBroker.cpp
@@ -0,0 +1,103 @@
+#include "PreCompile.h"
+#include <WinSock2.h>
+#include "MultiSocketRUDPCore.h"
+
+#if USE_IOCP_SESSION_BROKER
+bool MultiSocketRUDPCore::RUDPSessionBroker::Start(const std::wstring& sessionBrokerOptionFilePath)
+{
+	if (not CNetServer::Start(sessionBrokerOptionFilePath.c_str()))
+	{
+		std::cout << "RUDPSessionBroker Start failed" << std::endl;
+		return false;
+	}
+
+	return true;
+}
+
+void MultiSocketRUDPCore::RUDPSessionBroker::Stop()
+{
+	CNetServer::Stop();
+	isServerStopped = true;
+}
+
+void MultiSocketRUDPCore::RUDPSessionBroker::OnClientJoin(UINT64 clientId)
+{
+
+}
+
+void MultiSocketRUDPCore::RUDPSessionBroker::OnClientLeave(UINT64 sessionId)
+{
+
+}
+
+void MultiSocketRUDPCore::RUDPSessionBroker::OnRecv(UINT64 sessionId, NetBuffer* recvBuffer)
+{
+
+}
+
+void MultiSocketRUDPCore::RUDPSessionBroker::OnError(st_Error* OutError)
+{
+
+}
+#else
+
+void MultiSocketRUDPCore::RunSessionBrokerThread(PortType listenPort, std::string targetIP)
+{
+	SOCKET listenSocket, clientSocket = INVALID_SOCKET;
+
+	listenSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if (listenSocket == INVALID_SOCKET)
+	{
+		std::cout << "RunSessionBrokerThread listen socket is invalid with error " << WSAGetLastError() << std::endl;
+		return;
+	}
+
+	sockaddr_in serverAddr, clientAddr;
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_addr.S_un.S_addr = INADDR_ANY;
+	serverAddr.sin_port = htons(listenPort);
+	int sockAddrSize = static_cast<int>(sizeof(clientAddr));
+
+	if (bind(listenSocket, (sockaddr*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR)
+	{
+		std::cerr << "RunSessionBrokerThread bind failed with error " << WSAGetLastError() << std::endl;
+		closesocket(listenSocket);
+		return;
+	}
+
+	auto &buffer = *NetBuffer::Alloc();
+	listen(listenSocket, SOMAXCONN);
+	while (not threadStopFlag)
+	{
+		clientSocket = accept(listenSocket, (sockaddr*)&clientAddr, &sockAddrSize);
+		if (clientSocket == INVALID_SOCKET)
+		{
+			std::cout << "RunSessionBrokerThread accept falid with error " << WSAGetLastError() << std::endl;
+			continue;
+		}
+
+		PortType targetPort;
+
+		//Send rudp session infomation packet to client
+		buffer << targetIP << targetPort;
+
+		buffer.m_iWriteLast = buffer.m_iWrite;
+		buffer.m_iWrite = 0;
+		buffer.m_iRead = 0;
+		buffer.Encode();
+
+		int result = send(clientSocket, buffer.GetBufferPtr(), buffer.GetUseSize() + df_HEADER_SIZE, 0);
+		if (result == SOCKET_ERROR)
+		{
+			std::cout << "RunSessionBrokerThread send failed with error " << WSAGetLastError() << std::endl;
+			continue;
+		}
+
+		closesocket(clientSocket);
+		buffer.Init();
+	}
+
+	closesocket(listenSocket);
+	std::cout << "Session broker thread stopped" << std::endl;
+}
+#endif
