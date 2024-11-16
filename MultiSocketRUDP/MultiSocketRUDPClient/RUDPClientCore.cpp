104c2faf17e797d7ad51a3cec35c6f6cac7ca075
m5623skhj <m5623skhj@gmail.com>
2024-11-16 14:40:04 +0900

    * SessionGetter 추가 ** 연관 메서드 추가

--- MultiSocketRUDP/MultiSocketRUDPClient/RUDPClientCore.cpp
+++ MultiSocketRUDP/MultiSocketRUDPClient/RUDPClientCore.cpp
@@ -6,3 +6,49 @@ RUDPClientCore& RUDPClientCore::GetInst()
 	static RUDPClientCore instance;
 	return instance;
 }
+
+bool RUDPClientCore::Start(const std::wstring& optionFilePath)
+{
+	return sessionGetter.Start(optionFilePath);
+}
+
+void RUDPClientCore::Stop()
+{
+}
+
+bool RUDPClientCore::SessionGetter::Start(const std::wstring& optionFilePath)
+{
+	if (CNetClient::Start(optionFilePath.c_str()) == false)
+	{
+		std::cout << "SessionGetter::Start failed" << std::endl;
+		return false;
+	}
+
+	return true;
+}
+
+void RUDPClientCore::SessionGetter::OnConnectionComplete()
+{
+}
+
+void RUDPClientCore::SessionGetter::OnRecv(CNetServerSerializationBuf* recvBuffer)
+{
+
+}
+
+void RUDPClientCore::SessionGetter::OnSend(int sendSize)
+{
+}
+
+void RUDPClientCore::SessionGetter::OnWorkerThreadBegin()
+{
+}
+
+void RUDPClientCore::SessionGetter::OnWorkerThreadEnd()
+{
+}
+
+void RUDPClientCore::SessionGetter::OnError(st_Error* error)
+{
+	std::cout << "sessionGetter::OnError : ServerErr " << error->ServerErr << " GetLastError " << error->GetLastErr << std::endl;
+}
--- MultiSocketRUDP/MultiSocketRUDPClient/RUDPClientCore.h
+++ MultiSocketRUDP/MultiSocketRUDPClient/RUDPClientCore.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "NetClient.h"
 
 class RUDPClientCore
 {
@@ -10,4 +11,26 @@ private:
 
 public:
 	static RUDPClientCore& GetInst();
+
+public:
+	bool Start(const std::wstring& optionFilePath);
+	void Stop();
+
+private:
+	class SessionGetter : public CNetClient
+	{
+	public:
+		bool Start(const std::wstring& optionFilePath);
+
+	private:
+		virtual void OnConnectionComplete();
+		virtual void OnRecv(CNetServerSerializationBuf* recvBuffer);
+		virtual void OnSend(int sendsize);
+
+		virtual void OnWorkerThreadBegin();
+		virtual void OnWorkerThreadEnd();
+		virtual void OnError(st_Error* error);
+	};
+
+	SessionGetter sessionGetter;
 };
