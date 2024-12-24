6347cbe07da13a323e971ce7cac9c4c19804a459
m5623skhj <m5623skhj@gmail.com>
2024-12-25 00:40:53 +0900

    * 클라이언트 옵션 파일 추가 * 클라이언트 메인 루프 추가 * 클라이언트 프로젝트 속성 수정

--- MultiSocketRUDP/ContentsClient/main.cpp
+++ MultiSocketRUDP/ContentsClient/main.cpp
@@ -1,6 +1,25 @@
-#include <iostream>
+#include "PreCompile.h"
+#include "RUDPClientCore.h"
 
 int main()
 {
+	if (not RUDPClientCore::GetInst().Start(L"OptionFile/CoreOption.txt"))
+	{
+		std::cout << "Core start failed" << std::endl;
+		return 0;
+	}
+
+	while (true)
+	{
+		Sleep(1000);
+
+		if (GetAsyncKeyState(VK_ESCAPE) & 0x8000)
+		{
+			RUDPClientCore::GetInst().Stop();
+			break;
+		}
+	}
+	std::cout << "Server stop" << std::endl;
+
 	return 0;
 }
