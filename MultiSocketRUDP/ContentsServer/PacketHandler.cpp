b428f50d42fdd764e6a306ff2269f0775b2eff96
m5623skhj <m5623skhj@gmail.com>
2025-01-03 00:04:54 +0900

    * 귀찮아서 GET_PC() 함수 추가

--- MultiSocketRUDP/ContentsServer/PacketHandler.cpp
+++ MultiSocketRUDP/ContentsServer/PacketHandler.cpp
@@ -5,20 +5,33 @@
 #include "RUDPSession.h"
 #include "PC.h"
 
+#define GET_PC(pc, session) \
+	PCManager::GetInst().FindPC(session.GetSessionId()); \
+	if (pc == nullptr) \
+	{ \
+		return false; \
+	}
+
 namespace ContentsPacketHandler
 {
 	bool HandlePacket(RUDPSession& session, Ping& packet)
 	{
+		auto pc = GET_PC(pc, session);
+
 		return true;
 	}
 
 	bool HandlePacket(RUDPSession& session, TestStringPacket& packet)
 	{
+		auto pc = GET_PC(pc, session);
+
 		return true;
 	}
 
 	bool HandlePacket(RUDPSession& session, TestPacketReq& packet)
 	{
+		auto pc = GET_PC(pc, session);
+
 		return true;
 	}
 
