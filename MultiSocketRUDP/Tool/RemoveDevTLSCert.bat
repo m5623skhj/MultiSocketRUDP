9e959ce7700656331c757c10c8ecdad5ffb86822
DESKTOP-OC5B577\KimHyeongJin <m5623skhj@gmail.com>
2025-10-29 03:18:26 +0900

    * 배치 파일이 정상 동작하도록 수정

--- MultiSocketRUDP/Tool/RemoveDevTLSCert.bat
+++ MultiSocketRUDP/Tool/RemoveDevTLSCert.bat
@@ -1,15 +1,12 @@
 @echo off
 
-powershell -NoProfile -ExecutionPolicy Bypass -Command ^
-"Get-ChildItem -Path Cert:\LocalMachine\My | Where-Object {$_.Subject -like '*CN=DevServerCert*'} | Remove-Item"
+echo Deleting development TLS certificate...
+powershell -NoProfile -ExecutionPolicy Bypass -Command "Get-ChildItem Cert:\LocalMachine\My | Where-Object { $_.Subject -eq 'CN=DevServerCert' } | Remove-Item"
 
-IF %ERRORLEVEL% EQU 0
-(
-    echo CN=DevServerCert 인증서 삭제 완료.
-)
-ELSE
-(
-    echo 인증서 삭제 실패. 관리자 권한과 PowerShell 실행 정책을 확인하세요.
+IF %ERRORLEVEL% EQU 0 (
+    echo Certificate deletion successful
+) ELSE (
+    echo Certificate deletion failed
 )
 
 pause
