021f482094ed58de38d54f66b6295bd45d2c66a0
DESKTOP-OC5B577\KimHyeongJin <m5623skhj@gmail.com>
2025-11-27 22:09:11 +0900

    * std::cout 로거로 대체

--- MultiSocketRUDP/ContentsServer/Player.cpp
+++ MultiSocketRUDP/ContentsServer/Player.cpp
@@ -1,6 +1,9 @@
 #include "PreCompile.h"
 #include "Player.h"
 
+#include "LogExtension.h"
+#include "Logger.h"
+
 namespace Local
 {
 	static std::atomic_int16_t playerCount = 0;
@@ -15,11 +18,11 @@ Player::Player(MultiSocketRUDPCore& inCore)
 void Player::OnConnected()
 {
 	const auto nowPlayerCount = Local::playerCount.fetch_add(1, std::memory_order_relaxed) + 1;
-	std::cout << "Player connected. Current player count: " << nowPlayerCount << '\n';
+	LOG_DEBUG(std::format("Player connected. Current player count {}", nowPlayerCount));
 }
 
 void Player::OnDisconnected()
 {
 	const auto nowPlayerCount = Local::playerCount.fetch_sub(1, std::memory_order_relaxed) - 1;
-	std::cout << "Player disconnected. Current player count: " << nowPlayerCount << '\n';
+	LOG_DEBUG(std::format("Player disconnected. Current player count {}", nowPlayerCount));
 }
