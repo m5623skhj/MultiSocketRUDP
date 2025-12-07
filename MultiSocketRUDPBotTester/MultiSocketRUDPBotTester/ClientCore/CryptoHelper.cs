ff9b2300612ff40f204e5e5373fa32f2c6669590
DESKTOP-OC5B577\KimHyeongJin <m5623skhj@gmail.com>
2025-12-07 19:45:18 +0900

    * AESGCM 암복호화 함수 추가

--- MultiSocketRUDPBotTester/MultiSocketRUDPBotTester/ClientCore/CryptoHelper.cs
+++ MultiSocketRUDPBotTester/MultiSocketRUDPBotTester/ClientCore/CryptoHelper.cs
@@ -1,13 +1,42 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Security.Cryptography;
 
 namespace MultiSocketRUDPBotTester.ClientCore
 {
-    public class CryptoHelper
+    public static class CryptoHelper
     {
+        public static void Encrypt(byte[] key
+            , byte[] nonce
+            , byte[] plaintext
+            , byte[] add
+            , out byte[] ciphertext
+            , out byte[] tag)
+        {
+            ciphertext = new byte[plaintext.Length];
+            tag = new byte[16];
 
+            using var aesGcm = new AesGcm(key, tag.Length);
+            aesGcm.Encrypt(nonce, plaintext, ciphertext, tag, add);
+        }
+
+        public static byte[]? Decrypt(byte[] key
+            , byte[] nonce
+            , byte[] ciphertext
+            , byte[] add
+            , byte[] tag)
+        {
+            var plaintext = new byte[ciphertext.Length];
+
+            try
+            {
+                using var aesGcm = new AesGcm(key, tag.Length);
+                aesGcm.Decrypt(nonce, ciphertext, tag, plaintext, add);
+                return plaintext;
+            }
+            catch (CryptographicException)
+            {
+                return null;
+            }
+        }
     }
 }
