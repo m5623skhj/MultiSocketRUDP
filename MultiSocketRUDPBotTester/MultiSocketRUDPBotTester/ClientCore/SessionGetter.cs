2b2de32581cd71ea1788662d94ffb6b1f16d7643
DESKTOP-OC5B577\KimHyeongJin <m5623skhj@gmail.com>
2025-12-07 17:30:34 +0900

    * SessionGetter 추가   * Send는 할 필요 없으므로, ReceiveAsync만 구현

--- MultiSocketRUDPBotTester/MultiSocketRUDPBotTester/ClientCore/SessionGetter.cs
+++ MultiSocketRUDPBotTester/MultiSocketRUDPBotTester/ClientCore/SessionGetter.cs
@@ -1,12 +1,54 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Net.Security;
+using System.Net.Sockets;
+using System.Security.Authentication;
 
 namespace MultiSocketRUDPBotTester.ClientCore
 {
-    internal class SessionGetter
+    public class SessionGetter
     {
+        private TcpClient tcpClient;
+        private SslStream sslStream;
+
+        public async Task ConnectAsync(string host, int port, string? certFingerprint = null)
+        {
+            tcpClient = new TcpClient();
+            await tcpClient.ConnectAsync(host, port);
+
+            sslStream = new SslStream(tcpClient.GetStream()
+                , false
+                , (sender, certificate, chain, SslPolicyErrors) =>
+                {
+                    if (certFingerprint == null)
+                    {
+                        return true;
+                    }
+
+                    if (certificate == null)
+                    {
+                        return false;
+                    }
+
+                    var serverFp = certificate.GetCertHashString();
+                    return string.Equals(serverFp, certFingerprint, StringComparison.OrdinalIgnoreCase);
+
+                });
+
+            await sslStream.AuthenticateAsClientAsync(
+                targetHost: host,
+                clientCertificates: null,
+                enabledSslProtocols: SslProtocols.Tls12 | SslProtocols.Tls13,
+                checkCertificateRevocation: true);
+        }
+
+        public async Task<int> ReceiveAsync(byte[] buffer)
+        {
+            return await sslStream.ReadAsync(buffer, 0, buffer.Length);
+        }
+
+        public void Close()
+        {
+            sslStream?.Close();
+            tcpClient?.Close();
+        }
     }
 }
