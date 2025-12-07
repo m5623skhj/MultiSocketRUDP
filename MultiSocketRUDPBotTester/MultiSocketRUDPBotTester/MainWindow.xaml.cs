508135d012e601800626b162424d8811bb1ef9b4
DESKTOP-OC5B577\KimHyeongJin <m5623skhj@gmail.com>
2025-12-08 00:01:56 +0900

    * UI 수정   * 버튼 추가   * 입력 필드 추가

--- MultiSocketRUDPBotTester/MultiSocketRUDPBotTester/MainWindow.xaml.cs
+++ MultiSocketRUDPBotTester/MultiSocketRUDPBotTester/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using ClientCore;
+using MultiSocketRUDPBotTester.ClientCore;
 using System.Collections.ObjectModel;
 using System.Windows;
 using System.Windows.Threading;
@@ -18,13 +19,41 @@ namespace MultiSocketRUDPBotTester
             InitializeComponent();
 
             core = new MultiSocketRUDPBotCore();
-            Logs = new ObservableCollection<string>();
+            Logs = [];
             DataContext = this;
 
-            DispatcherTimer timer = new DispatcherTimer();
-            timer.Interval = TimeSpan.FromSeconds(1);
+            var timer = new DispatcherTimer
+            {
+                Interval = TimeSpan.FromSeconds(1)
+            };
             timer.Tick += (s, e) => UpdateUI();
             timer.Start();
+        }
+
+        private void StartBotTest_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var numOfBot = UInt16.Parse(BotCountTextBox.Text);
+                HostIpTextBox.IsEnabled = false;
+                HostPortTextBox.IsEnabled = false;
+                BotCountTextBox.IsEnabled = false;
+
+                BotTesterCore.Instance.StartBotTest(numOfBot);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"error {ex.Message}");
+            }
+        }
+
+        private void StopBotTest_Click(object sender, RoutedEventArgs e)
+        {
+            BotTesterCore.Instance.StopBotTest();
+
+            HostIpTextBox.IsEnabled = true;
+            HostPortTextBox.IsEnabled = true;
+            BotCountTextBox.IsEnabled = true;
         }
 
         private void UpdateUI()
