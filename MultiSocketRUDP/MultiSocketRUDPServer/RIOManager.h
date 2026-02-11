0c8ddd50191720fd164b1b8f4e15f3bff8e3b8ff
DESKTOP-OC5B577\KimHyeongJin <m5623skhj@gmail.com>
2026-02-12 01:40:02 +0900

    * 주석 추가

--- MultiSocketRUDP/MultiSocketRUDPServer/RIOManager.h
+++ MultiSocketRUDP/MultiSocketRUDPServer/RIOManager.h
@@ -41,6 +41,20 @@ public:
 		PRIO_BUF flagsContext,
 		ULONG flags,
 		PVOID requestContext) const;
+
+	// ----------------------------------------
+	// @brief 버퍼에 있는 데이터를 원격 대상으로 전송합니다.
+	// @param rioRQ RIO 요청 큐 핸들입니다.
+	// @param rioBuffer 송신할 데이터 버퍼입니다.
+	// @param bufferCount rioBuffer에 있는 RIO_BUF 구조체의 수입니다.
+	// @param localAddr 송신에 사용될 로컬 주소 정보입니다.
+	// @param remoteAddr 원격 대상의 주소 정보입니다.
+	// @param controlContext 제어 정보를 포함하는 버퍼입니다.
+	// @param flagsContext 플래그 정보를 포함하는 버퍼입니다.
+	// @param flags 송신 작업의 동작을 제어하는 플래그입니다.
+	// @param requestContext 작업 완료 시 반환될 컨텍스트 값입니다.
+	// @return 함수 성공 시 true, 실패 시 false를 반환합니다.
+	// ----------------------------------------
 	[[nodiscard]]
 	bool RIOSendEx(const RIO_RQ& rioRQ,
 		PRIO_BUF rioBuffer,
