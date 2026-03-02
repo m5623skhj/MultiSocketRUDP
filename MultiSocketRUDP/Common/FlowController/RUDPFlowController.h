b0fe41a9eed65054d70e359d0a80fa4a9317e9bd
DESKTOP-OC5B577\KimHyeongJin <m5623skhj@gmail.com>
2026-03-02 18:41:46 +0900

    * 주석 추가

--- MultiSocketRUDP/Common/FlowController/RUDPFlowController.h
+++ MultiSocketRUDP/Common/FlowController/RUDPFlowController.h
@@ -23,6 +23,14 @@ public:
 	PacketSequence GetLastAckedSequence() const noexcept { return lastReplySequence; }
 
 private:
+	// ----------------------------------------
+	// @brief 두 패킷 시퀀스 번호의 차이를 계산합니다.
+	// 시퀀스 번호는 순환하므로, 직접적인 뺄셈이 아닌 흐름 제어 로직에 맞는
+	// 상대적인 차이를 반환하여 순서가 뒤바뀌거나 누락된 패킷을 감지하는 데 사용됩니다.
+	// @param a 첫 번째 패킷 시퀀스.
+	// @param b 두 번째 패킷 시퀀스.
+	// @return 두 시퀀스 번호의 차이.양수면 a가 b보다 나중 시퀀스임을 나타냅니다.
+	// ----------------------------------------
 	static int32_t SeqDiff(PacketSequence a, PacketSequence b) noexcept;
 
 private:
